b3f3273004096b81f42b659cf283e45d52b28c17
CBNUKE <jimmy29922@gmail.com>
2017-10-17 01:43:55 +0700

    Update Laravel mix and Add datatables

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,15 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.js('node_modules/datatables.net/js/jquery.dataTables.js', 'public/js')
+    .copy('node_modules/datatables.net-bs/css/dataTables.bootstrap.css', 'public/css')
+    .js('node_modules/datatables.net-bs/js/dataTables.bootstrap.js', 'public/js')
+    .js('node_modules/datatables.net-buttons/js/buttons.colVis.js', 'public/js')
+    .js('node_modules/datatables.net-buttons/js/buttons.flash.js', 'public/js')
+    .js('node_modules/datatables.net-buttons/js/buttons.html5.js', 'public/js')
+    .js('node_modules/datatables.net-buttons/js/buttons.print.js', 'public/js')
+    .js('node_modules/datatables.net-buttons/js/dataTables.buttons.js', 'public/js')
+    .copy('node_modules/datatables.net-buttons-bs/css/buttons.bootstrap.css', 'public/css')
+    .js('node_modules/datatables.net-buttons-bs/js/buttons.bootstrap.js', 'public/js');
